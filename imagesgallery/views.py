0e7af3fd4f0aa7f2e986f67613945367641fe0fb
Loise Mwarangu <loisemwarangu@gmail.com>
2019-03-04 22:34:34 +0300

    added search and images to views and linked urls

--- imagesgallery/views.py
+++ imagesgallery/views.py
@@ -14,4 +14,25 @@ def photo_date(request):
     return render(request, 'all-photos/recent.html', {"date": date,})
 
 def posted_photos(request):
-    return render(request, 'all-photos/index.html')
+    images = Image.all_images()
+    return render(request, 'all-photos/index.html' , {"images":images})
+  
+def search_results(request):
+
+    if 'photo' in request.GET and request.GET["photo"]:
+        search_term = request.GET.get("photo")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+    else:
+        message = "hate to break it to you but this category has no photos yet "
+        return render(request, 'search.html',{"message":message})
+    return render(request,"search.html")    
+
+
+def photo(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"index.html",{"image":image})
