c3117e81335457866d782a4d8ae75d7bc5e714f1
Loise Mwarangu <loisemwarangu@gmail.com>
2019-03-06 12:46:21 +0300

    solved test errors

--- imagesgallery/urls.py
+++ imagesgallery/urls.py
@@ -7,8 +7,7 @@ from . import views
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url('^recent/$',views.photo_date,name='photosToday'), 
-    url(r'^search/', views.search, name='search_results'),
-    url(r'^image/(\d+)',views.posted_photos,name='posted_photos')
+    url(r'^search/$', views.search, name='search_results'),
 ]
 
 if settings.DEBUG:
